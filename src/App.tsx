61c4e80a0709cce590e1cd6a7ff7bcc002eaaa8b
gasangw <thomasgasangwa@gmail.com>
2024-10-30 15:38:12 +0200

    chore: install react icons package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="text-red-700 text-center">Hello</div>;
+  return (
+    <div className="bg-slate-500/30 *:text-white flex justify-center min-h-screen p-8">
+       <div>
+         <h1 className="font-bold text-2xl">Speech Color Changer</h1>
+           <button></button>
+       </div>
+    </div>
+  );
 }
 
 export default App;
